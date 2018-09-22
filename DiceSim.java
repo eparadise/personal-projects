5a199c76167db38f16930fe1c76db4cbc53f5e62
eparadise <43389857+eparadise@users.noreply.github.com>
2018-09-22 13:53:02 -0400

    Create DiceSim.java

---
+++ DiceSim.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+public class DiceSim {
+	public static int Dice() {
+		int num = 0;
+		int min = 1;
+		int max = 6;
+		
+		/*Algorithm 1
+		 * Uses a Random object to generate numbers
+		Random rand = new Random();
+		num = rand.nextInt(max + 1);
+		return num;  */
+		
+		/* Algorithm 2
+		 * Makes a list with the die values in it and shuffles it, returning whichever integer ends up at the first index
+		 ArrayList<Integer> die = new ArrayList<>();
+		 
+		for (int i = min; i < max + 1; i++) {
+			die.add(i);
+		}
+		Collections.shuffle(die);
+		num = die.get(0);
+		return num;
+		*/
+		return num;
+	} 
+	
+	public static void main (String[] args) {
+	for (int i = 0; i < 6; i++) {
+		System.out.println(Dice());
+	}
+	}
+
+}
