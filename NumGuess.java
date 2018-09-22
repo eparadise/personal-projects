272888b2f4a9ca5ada3bb6fc808e41d0e11412a3
eparadise <43389857+eparadise@users.noreply.github.com>
2018-09-22 13:53:56 -0400

    Create NumGuess.java

---
+++ NumGuess.java
@@ -0,0 +1,52 @@
+import java.util.Random;
+import java.util.Scanner;
+public class NumGuess {
+	public static void Guess() {
+		int max = 100;
+		int numGuesses = 0;
+		Random rand = new Random();
+		int myNum = rand.nextInt(max + 1);
+		int user = 0;
+		System.out.println("I'm thinking of a number between 1 and 100. Think you can guess it? Enter a guess below.");
+		Scanner scan = new Scanner(System.in);
+
+		while (!scan.hasNextInt()) {
+			System.out.println("Please enter an Integer.");
+			scan.next();
+		}
+		user = scan.nextInt();
+		numGuesses++;
+		while (user != myNum) {
+			if (user > myNum) {
+				System.out.println("Your guess is too high. Try again.");
+			}
+			if (user < myNum) {
+				System.out.println("Your guess is too low. Try again.");
+			}
+			while (!scan.hasNextInt()) {
+				System.out.println("Please enter an integer.");
+				scan.next();
+			}
+			user = scan.nextInt();
+			numGuesses++;
+		}
+
+		System.out.println("Congrats! You guessed my number in " + numGuesses + " guesses.");
+		if (numGuesses <= 5) {
+			System.out.println("You're a pro!");
+		}
+		else if (numGuesses <= 10) {
+			System.out.println("Not bad!");
+		}
+		else {
+			System.out.println("You can do better than that!");
+		}
+		scan.close();
+	}
+
+
+	public static void main(String[] args) {
+		Guess();
+	}
+}
+
